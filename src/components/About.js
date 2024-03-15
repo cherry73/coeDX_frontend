5a5f2a222c0255fc9457800bf834c6bdd9c9d63a
“cherry73” <sree.charan73@gmail.com>
2024-03-15 18:19:03 +0100

    1st Edit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+//import { Icon } from "@iconify/react";
+//import angularIcon from "@iconify/icons-logos/angular-icon";
+//import reactIcon from "@iconify/icons-logos/react";
+//import vueIcon from "@iconify/icons-logos/vue";
 
 class About extends Component {
   render() {
@@ -28,20 +28,9 @@ class About extends Component {
                   <img
                     height="250px"
                     src={profilepic}
-                    alt="Avatar placeholder"
+                    alt=""
                   />
-                  <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={vueIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
+                  
                 </span>
               </div>
             </div>
